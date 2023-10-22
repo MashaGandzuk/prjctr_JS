6c5533e67ecdbb33b17da16edc6f348a2f1641e5
MashaGandzuk <66442192+MashaGandzuk@users.noreply.github.com>
2023-10-22 19:25:25 +0300

    first commit

---
+++ hw1/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+console.log(b - a);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(b ** a);
+console.log(Math.sqrt(a));
+
+let c = "JavaScript";
+
+console.log(c.toString());
+console.log(Number(c));
+console.log(Boolean(c));
+console.log(!!c);
