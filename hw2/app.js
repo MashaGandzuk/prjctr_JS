3fe88703536cf8e2ed7e563c126e2c66c061bcac
MashaGandzuk <66442192+MashaGandzuk@users.noreply.github.com>
2023-10-25 16:37:57 +0300

    hw 2

---
+++ hw2/app.js
@@ -0,0 +1,34 @@
+for (i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("ЛолКек");
+  } else if (i % 3 === 0) {
+    console.log("Лол");
+  } else if (i % 5 === 0) {
+    console.log("Кек");
+  } else console.log(i);
+}
+
+const value1 = +prompt("Введіть число", "");
+
+if (isNaN(value1) || typeof value1 !== "number") {
+  console.log("Таке чуство шо Бог десь наказує нас за шось");
+} else {
+  for (j = 1; j < value1; j++) {
+    if (j % 2 === 0) {
+      console.log(j);
+    }
+  }
+}
+
+const value2 = 10;
+let n = 1;
+if (typeof value2 !== "number") {
+  console.log("Таке чуство шо Бог десь наказує нас за шось");
+} else {
+  while (n < value2) {
+    if (n % 2 === 0) {
+      console.log(n);
+    }
+    n++;
+  }
+}
