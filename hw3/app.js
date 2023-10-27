86b18c47bf854ce6d085cb19cc248adcfdc5c97b
MashaGandzuk <66442192+MashaGandzuk@users.noreply.github.com>
2023-10-27 14:03:39 +0300

    hw 3

--- hw3/app.js
+++ hw3/app.js
@@ -1,8 +1,28 @@
-// function recursiveOddSumTo(number) {
-//   if (number % 2 !== 0) {
-//     return number;
-//   }
-// }
+"use strict";
 
-// console.log(recursiveOddSumTo(1)); // 1
-// console.log(recursiveOddSumTo(10)); // 25
+// 1
+
+function recursiveOddSumTo(number) {
+  if (number === 1) {
+    return number;
+  }
+  let oddNumber = number % 2 === 0 ? number - 1 : number;
+  return oddNumber + recursiveOddSumTo(oddNumber - 2);
+}
+console.log(recursiveOddSumTo(1)); // 1
+console.log(recursiveOddSumTo(10)); // 25
+
+// 2
+
+function iterativeOddSumTo(number) {
+  let result = 0;
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 === 1) {
+      result += i;
+    }
+  }
+  return result;
+}
+
+console.log(iterativeOddSumTo(1)); // 1
+console.log(iterativeOddSumTo(10)); // 25
