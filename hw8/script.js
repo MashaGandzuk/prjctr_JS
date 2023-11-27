930f8a9dc04930e3df5dca9b30616ee645c2aadc
MashaGandzuk <66442192+MashaGandzuk@users.noreply.github.com>
2023-11-27 17:18:55 +0200

    hw8

---
+++ hw8/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const navigationDom = document.querySelector("#headerTwo");
+console.log(navigationDom);
+
+const firstSection = document.querySelectorAll("section")[0];
+console.log(firstSection);
+
+const liItem5 = document.getElementsByTagName("li")[4];
+console.log(liItem5);
+
+const hatredLevelBlock = document.getElementsByClassName("hatredLevelBlock");
+console.log(hatredLevelBlock);
